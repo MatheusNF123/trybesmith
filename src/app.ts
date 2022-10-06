bc2d774225c0d0323a02be96bce2c89a1ba493a7
Matheus <matheus_cs.nf@hotmail.com>
2022-10-06 20:08:00 -0300

    exercicio 5

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from 'express';
+
 import errorMidleware from './middlewares/Error';
+
 import productRouter from './routes/product.route'
+
 import userRouter from './routes/user.route'
+
 import orderRouter from './routes/order.route'
+
 import LoginRouter from './routes/login.route'
+
 const app = express();
 
 app.use(express.json());
 
-app.use('/products', productRouter)
-app.use('/users', userRouter)
-app.use('/orders', orderRouter)
-app.use('/login', LoginRouter)
-app.use(errorMidleware)
-//
+app.use('/products', productRouter);
+app.use('/users', userRouter);
+app.use('/orders', orderRouter);
+app.use('/login', LoginRouter);
+app.use(errorMidleware);
 
 export default app;
